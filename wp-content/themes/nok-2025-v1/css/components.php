22fb4d98617999ca0d7869e6ed391e4cba4fd74a
Klaas Leussink <hnldesign@hnldesign.nl>
2025-05-07 15:44:06 +0200

    Wrote a new universal scrollbar handler (nok-scrollbar) that emulates a scrollbar for a more uniform look across all devices, and also offers dragging and auto-scrolling, nullifying the need for hnl.draggable.mjs and nok-autoscroll.mjs (removed). Starting to restore order in the various gap assignments. Not done yet. Block group has been deprecated and removed, in favor of a regular nok-layout-grid container, combined with scrollable and columns-to-slides. If need for block group comes up, we should solve this using the layout handlers for grids.

--- wp-content/themes/nok-2025-v1/css/components.php
+++ wp-content/themes/nok-2025-v1/css/components.php
@@ -380,11 +380,11 @@ $logo = '<nok-logo><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 463.58 1
                     reprehenderit vel voluptatum. Lorem ipsum dolor sit amet, consectetur adipisicing elit!
                 </p>
 
-                <nok-block-group class="nok-draggable__horizontal nok-mt-2" data-requires="./modules/hnl.draggable"
-                     data-snap-items="nok-square-block" data-scroll-snapping="true">
-                    <div class="block_container draggable-container">
+                <div class="nok-mt-2 span-all-columns">
+                    <div class="nok-layout-grid nok-layout-grid__3-column
+                nok-scrollable__horizontal columns-to-slides" data-scroll-snapping="true" data-draggable="true" data-autoscroll="true">
                         <?php $x = 6; while ($x--) : ?>
-                            <nok-square-block class="nok-draggable-slider-item nok-bg-darkblue">
+                            <nok-square-block class="nok-bg-darkblue">
                                 <div class="nok-square-block__icon">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 13" fill="currentColor">
                                         <path d="M4.578.004c-.398.02-.777.094-1.133.223a3.803 3.803 0 0 0-2.058 1.746A4.127 4.127 0 0 0 .956 3.2a4.602 4.602 0 0 0-.047.976c.042.58.209 1.145.488 1.656.059.106.153.16.27.16.137 0 .25-.09.285-.226a.318.318 0 0 0 0-.149q-.008-.031-.082-.176a3.474 3.474 0 0 1-.367-1.77c.023-.335.082-.62.187-.913.36-1.02 1.2-1.79 2.239-2.051.187-.047.359-.078.562-.094.11-.008.41-.008.52 0a3.361 3.361 0 0 1 2.034.895c.024.023.305.3.626.625.418.422.586.586.601.597.054.034.117.05.18.047.07 0 .117-.011.172-.05.016-.008.226-.22.617-.614.328-.328.617-.617.645-.64.272-.248.583-.448.921-.594.352-.149.7-.234 1.098-.27.078-.004.395-.004.477 0a3.249 3.249 0 0 1 3.011 3.227c.014.864-.3 1.702-.879 2.344-.05.058-.449.46-1.761 1.777-3.102 3.113-4.301 4.316-4.309 4.316 0 0-.07-.066-.152-.148-.086-.086-.3-.3-.48-.477-.829-.828-1.876-1.87-2.899-2.902-.328-.324-.461-.457-.48-.473a.275.275 0 0 0-.098-.043.361.361 0 0 0-.145.004.298.298 0 0 0-.215.22.452.452 0 0 0 0 .144c.012.04.03.079.055.113.04.047 3.996 4.004 4.192 4.191.045.049.105.08.171.09.04.008.09.008.13-.004a.456.456 0 0 0 .109-.05c.035-.032.433-.43 4.203-4.211a212.272 212.272 0 0 0 2.11-2.125 4.02 4.02 0 0 0 1.035-3.055 3.879 3.879 0 0 0-.872-2.16A3.844 3.844 0 0 0 12.144 0c-.886 0-1.753.3-2.453.852-.18.14-.222.183-.758.722l-.484.485-.433-.434a19.35 19.35 0 0 0-.637-.629 4.002 4.002 0 0 0-2.39-.988 7.13 7.13 0 0 0-.411-.004Z"/>
@@ -408,10 +408,7 @@ $logo = '<nok-logo><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 463.58 1
                             </nok-square-block>
                         <?php endwhile; ?>
                     </div>
-                    <div class="fake-scrollbar align-self-stretch">
-                        <div class="fake-scrollbar-thumb"></div>
-                    </div>
-                </nok-block-group>
+                </div>
 
             </article>
 
@@ -422,7 +419,7 @@ $logo = '<nok-logo><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 463.58 1
 <nok-section>
     <div class="nok-section__inner--stretched">
         <div class="nok-section__inner">
-            <article class="nok-layout-grid nok-layout-grid__2-column fill-fill nok-align-items-start nok-gap-3">
+            <article class="nok-layout-grid nok-layout-grid__2-column fill-fill nok-align-items-start">
                 <div class="nok-layout-flex-column nok-align-items-stretch">
                     <h1>
                         Ons veelzijdige team van specialisten
@@ -433,35 +430,31 @@ $logo = '<nok-logo><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 463.58 1
                         reprehenderit vel voluptatum. Lorem ipsum dolor sit amet, consectetur adipisicing elit!
                     </p>
 
-                    <nok-square-block class="nok-draggable__horizontal nok-p-2
-                    nok-bg-body--darker nok-dark-bg-darkblue nok-text-contrast" data-requires="./modules/hnl.draggable"
-                                      data-snap-items="nok-draggable-slider-item" data-scroll-snapping="true">
-                        <div class="draggable-container">
-                            <div class="nok-draggable-slider-item">
+                    <nok-square-block class="nok-p-2 no-gap
+                    nok-bg-body--darker nok-dark-bg-darkblue nok-text-contrast">
+                        <div class="nok-scrollable__horizontal" data-scroll-snapping="true" data-draggable="true" data-autoscroll="true">
+                            <div>
                                 <blockquote class="nok-square-block__text">
                                     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet doloribus iure perspiciatis quod, quos
                                     vero. Architecto, blanditiis est exercitationem illo iusto magni nihil nulla, quam quas, quia
                                     reprehenderit vel voluptatum. Lorem ipsum dolor sit amet, consectetur adipisicing elit!"
                                 </blockquote>
-                                <div class="nok-layout-flex-column nok-align-items-start no-flex-gap">
+                                <div class="nok-layout-flex-column nok-align-items-start no-gap">
                                     <strong class="nok-fs-2">John Doe</strong>
                                     <p>Internist</p>
                                 </div>
                             </div>
-                            <div class="nok-draggable-slider-item">
+                            <div>
                                 <blockquote class="nok-square-block__text">
                                     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet doloribus iure perspiciatis quod, quos
                                     vero. Architecto, blanditiis est exercitationem illo iusto magni nihil nulla, quam quas, quia
                                     reprehenderit vel voluptatum. Lorem ipsum dolor sit amet, consectetur adipisicing elit!"
                                 </blockquote>
-                                <div class="nok-layout-flex-column nok-align-items-start no-flex-gap">
+                                <div class="nok-layout-flex-column nok-align-items-start no-gap">
                                     <strong class="nok-fs-2">Jane Doe</strong>
                                     <p>Di&euml;tist</p>
                                 </div>
                             </div>
-                        </div>
-                        <div class="fake-scrollbar align-self-stretch">
-                            <div class="fake-scrollbar-thumb"></div>
                         </div>
                     </nok-square-block>
 
@@ -472,7 +465,7 @@ $logo = '<nok-logo><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 463.58 1
                                 vero. Architecto, blanditiis est exercitationem illo iusto magni nihil nulla, quam quas, quia
                                 reprehenderit vel voluptatum. Lorem ipsum dolor sit amet, consectetur adipisicing elit!"
                             </blockquote>
-                            <div class="nok-layout-flex-column nok-align-items-start no-flex-gap">
+                            <div class="nok-layout-flex-column nok-align-items-start no-gap">
                                 <strong class="nok-fs-2">John Doe</strong>
                                 <p>Internist</p>
                             </div>
@@ -510,7 +503,7 @@ $logo = '<nok-logo><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 463.58 1
     <div class="nok-section__inner--stretched nok-my-0 nok-px-0 nok-border-radius-0
     nok-bg-white nok-dark-bg-darkestblue nok-text-darkblue nok-dark-text-white">
         <div class="nok-section__inner nok-page-footer__inner">
-            <div class="nok-layout-grid nok-layout-grid__2-column fill-fill nok-align-items-center nok-gap-3">
+            <div class="nok-layout-grid nok-layout-grid__2-column fill-fill nok-align-items-start">
                 <nok-square-block class="nok-bg-body nok-dark-bg-body--darker nok-alpha-10 nok-alpha-10">
                     <div class="nok-square-block__heading">
                         <h3 class="fw-bold">Vragen?</h3>
