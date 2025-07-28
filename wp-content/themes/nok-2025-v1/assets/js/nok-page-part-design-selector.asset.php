9adc91ae0717f8f0633a738677a2342dd517981c
Klaas Leussink <hnldesign@hnldesign.nl>
2025-07-28 16:01:11 +0200

    Added new custom field: checkox. Returns 1 for checked, 0 for unchecked - both as string values. Updated todo list

--- wp-content/themes/nok-2025-v1/assets/js/nok-page-part-design-selector.asset.php
+++ wp-content/themes/nok-2025-v1/assets/js/nok-page-part-design-selector.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-plugins'), 'version' => 'ea3c1de8acf3188ea901');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-plugins'), 'version' => '2aecdff99a8205900f62');
--- wp-content/themes/nok-2025-v1/template-parts/page-parts/nok-plain-text-block.php
+++ wp-content/themes/nok-2025-v1/template-parts/page-parts/nok-plain-text-block.php
@@ -5,12 +5,14 @@
  * Slug: nok-plain-text-block
  *  Custom Fields:
  * - layout:select(left|center|right)
+ * - collapse_bottom:checkbox
  */
 
 $layout = ( $page_part_fields['layout'] ?? 'center' ) === "" ? 'center' : $page_part_fields['layout'];
+
 ?>
 
-    <nok-section>
+    <nok-section <?= ($page_part_fields['collapse_bottom'] === "1") ? 'class="collapse-bottom"' : ''; ?>>
         <div class="nok-section__inner">
             <article class="nok-layout-grid nok-layout-grid__1-column nok-justify-items-<?= $layout;?> nok-column-gap-3 text-<?= $layout;?>
                         nok-text-darkblue">
