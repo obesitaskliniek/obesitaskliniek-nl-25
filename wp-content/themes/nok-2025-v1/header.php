aeeb340b2140c3cc418b9ded6220f9fbbad4a46c
Klaas Leussink <hnldesign@hnldesign.nl>
2025-10-13 11:56:07 +0200

    BMI calculator now loads in "strict" lazy mode (doesn't load until actually visible) Reworked toggler to use more modern events for pointer events Did some cleaning up

--- wp-content/themes/nok-2025-v1/header.php
+++ wp-content/themes/nok-2025-v1/header.php
@@ -2,7 +2,7 @@
 <html lang="nl" class="no-js <?= (is_user_logged_in() ? 'logged-in' : ''); ?>">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover, interactive-widget=overlays-content">
 
         <title>Temp header</title>
 
@@ -11,8 +11,18 @@
         <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
               rel="stylesheet">
         <link href="<?= THEME_ROOT ;?>/assets/fonts/realist.css" rel="stylesheet" crossorigin="anonymous">
-        <link rel="modulepreload" href="<?= THEME_ROOT ;?>/assets/js/entrypoint.min.mjs">
-        <script type="module" src="<?= THEME_ROOT ;?>/assets/js/entrypoint.min.mjs" defer></script>
+
+        <!--<link rel="modulepreload" href="<?= THEME_ROOT ;?>/assets/js/mobileConsole/hnl.mobileconsole.js?cache=<?= time(); ?>">
+        <script type="module">
+            if (window.matchMedia('(max-width: 768px)').matches) {
+                import('<?= THEME_ROOT ;?>/assets/js/mobileConsole/hnl.mobileconsole.js?cache=<?= time(); ?>');
+            }
+        </script>
+        <!--<script src="<?= THEME_ROOT ;?>/assets/js/mobileConsole/hnl.mobileconsole.js?cache=<?= time(); ?>"></script><!-- -->
+
+
+        <link rel="modulepreload" href="<?= THEME_ROOT ;?>/assets/js/entrypoint.mjs">
+        <script type="module" src="<?= THEME_ROOT ;?>/assets/js/entrypoint.mjs" defer></script>
 
         <!-- head data -->
         <?php wp_head(); ?>
