1252607b1f3e60dc82d4c887c66bc837a7f74694
c-kick <hnldesign@hnldesign.nl>
2026-06-08 09:19:34 +0200

    feat(post-meta): amend meta panel asset

--- wp-content/themes/nok-2025-v1/assets/js/nok-post-meta-panel.asset.php
+++ wp-content/themes/nok-2025-v1/assets/js/nok-post-meta-panel.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-plugins'), 'version' => '7bbf9e74de71f531d909');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-plugins'), 'version' => '5bb472f75b295eac17e7');
